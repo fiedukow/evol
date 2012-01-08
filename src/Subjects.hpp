750f56ac0a52473e9a286aa4189662caf688d91f
Maciej 'mac' Grzybek <mashester@gmail.com>
2012-01-08 15:00:13 +0100

    Merging Subjects.hpp (some trival conflicts).

--- src/Subjects.hpp
+++ src/Subjects.hpp
@@ -1,6 +1,9 @@
 #ifndef _SUBJECTS_HPP_
 #define _SUBJECTS_HPP_
 
+namespace evol
+{
+
 /**
  * Class represents Subject (person, individual etc.)
  * Subject can:
@@ -12,6 +15,12 @@
  */
 class Subject
 {
+    private:
+    /**
+     * Vector of Chromosomes as subject attributes.
+     */
+    std::vector< std::shared_ptr<Chromosome> > chromosomes;
+
     public:
 
     /**
@@ -19,26 +28,39 @@ class Subject
      * @param  smart pointer to Subject which dereferences to Subject to crossover with.
      * @return smart pointer to Subject which represents product of crossover with Subject from argument, which is new Subject.
      */
-    shared_ptr<Subject> crossWith(shared_ptr<Subject> subject);
+    std::shared_ptr<Subject> crossWith(std::shared_ptr<Subject> &subject);
 
     /**
      * Do the mutation (self-modification) of subject.
      * @return smart pointer reference as a product of mutation, which is new Subject.
      */
-    shared_ptr<Subject> mutate();
-
-    private:
+    std::shared_ptr<Subject> mutate();
 
     /**
-     * Vector of Chromosomes as subject attributes.
+     * Add single chromosome to vector of subject's chromosomes.
+     * @param chromosome smart pointer to chromosome to be added
      */
-    Vector<shared_ptr<Chromosome> > chromosomes;
-    
+    void addChromosome(std::shared_ptr<Chromosome> &chromosome);
+
+    /**
+     * Get single chromosome by given id
+     * @param id position of chromosome in Subject's chromosome vector
+     * @return smart pointer to got chromosome
+     */
+    std::shared_ptr<Chromosome> getChromosome(unsigned int id) throw ChromosomeOutOfBoundException;
+
+    /**
+     * Replace whole vector of subject's chromosomes.
+     * @param chromosomes reference to vector of chromosomes that will replace current
+     */
+    void replaceChromosomes(std::Vector< std::shared_ptr<Chromosome> > &chromosomes);
+
 
     /*
      * TODO: setInitialValue(); - set all chromosomes as you like
      *       clone(); - deep copy
      */
+
 };
 
 /**
@@ -56,13 +78,15 @@ class Chromosome
      * @param chromosome - smart pointer to Chromosome which dereferences to Chromosome to crossover with.
      * @return smart pointer to Chromosome which represents product of crossover current chromosome with chromosome from argument, which is new Chromosome.
      */
-    virtual shared_ptr<Chromosome> crossWith(shared_ptr<Chromosome> chromsome); 
+    virtual std::shared_ptr<Chromosome> crossWith(std::shared_ptr<Chromosome> chromsome); 
 
     /**
      * Do the mutation (self-modification) of current Chromosome
      * @return smart pointer to Chromosome which represents product of mutation, which is new Chromosome.
      */
-    virtual shared_ptr<Chromosome> mutate();
+    virtual std::shared_ptr<Chromosome> mutate();
 };
+
+} /* end of namespace evol */
 
 #endif
